b19a98b9009cd34eb424ee7c35b29df9e2da87fa
Hebert <hebertcdev@gmail.com.br>
2023-07-19 08:50:58 -0300

    realizando expresão

--- lib/screens/widgets/botton_w.dart
+++ lib/screens/widgets/botton_w.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
 class BottonWidget extends StatelessWidget {
-  const BottonWidget({super.key, required this.botao});
+  const BottonWidget({
+    super.key,
+    required this.botao,
+    required this.expresao,
+  });
   final String botao;
+  final void Function(String digito)? expresao;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: () {
+        expresao!(botao);
+      },
       style: ElevatedButton.styleFrom(backgroundColor: const Color(0xFF0E2954)),
       child: Text(
         botao,
